ea6ac7cfcbccd04334a9724822276dc6634c7261
sallyretro <61210853+sallyretro@users.noreply.github.com>
2023-12-06 23:14:55 -0600

    Update createevent.php

--- createevent.php
+++ createevent.php
@@ -1,75 +1,120 @@
 <?php
 session_start();
-	include("serverconnection.php");
-	include("functions.php");
-	$user_data = valid_login($con);
-	if ($_SERVER['REQUEST_METHOD'] == "POST"){
+include("serverconnection.php");
+include("functions.php");
+$user_data = valid_login($con);
 
-		$name =$_POST['name'];
-		$location =$_POST['location'];
-		$tickets_available = $_POST['tickets_available'];
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $name = $_POST['name'];
+    $location = $_POST['location'];
+    $tickets_available = $_POST['tickets_available'];
 
-		if(!empty($name) && !empty($location) && !empty($tickets_available)){
-			$query = "INSERT INTO events (name, location, tickets_available) values('$name', '$location', '$tickets_available')";
-			mysqli_query($con, $query);
-			header("Location: index.php");
-			die;
-		}
-		else{
-			echo "Not an accepted combination";
-		}
-	}
+    if (!empty($name) && !empty($location) && !empty($tickets_available)) {
+        $query = "INSERT INTO events (name, location, tickets_available) VALUES (?, ?, ?)";
+        
+        $stmt = mysqli_prepare($con, $query);
+        mysqli_stmt_bind_param($stmt, "sss", $name, $location, $tickets_available);
+        mysqli_stmt_execute($stmt);
+
+        header("Location: index.php");
+        die;
+    } else {
+        echo "Not an accepted combination";
+    }
+}
 ?>
-
-
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Create an event!</title>
+    <title>Create an event!</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        #box {
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            padding: 20px;
+            max-width: 400px;
+            width: 100%;
+        }
+
+        #box h2 {
+            font-size: 24px;
+            color: #3498db;
+            margin: 0;
+            padding-bottom: 10px;
+        }
+
+        #box form {
+            margin-top: 10px;
+        }
+
+        .form-group {
+            margin-bottom: 15px;
+        }
+
+        .form-group label {
+            display: block;
+            font-size: 16px;
+            font-weight: bold;
+        }
+
+        .form-group input[type="text"],
+        .form-group input[type="number"] {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+
+        #button {
+            padding: 10px;
+            width: 100%;
+            color: white;
+            background-color: #3498db;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        #box a {
+            display: block;
+            text-decoration: none;
+            font-size: 16px;
+            color: #3498db;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-
-<style type="text/css">
-	#text{
-
-		height: 25px;
-		border-radius: 5px;
-		padding: 4px;
-		border: solid thin #aaa;
-		width: 100%;
-	}
-
-	#button{
-
-		padding: 10px;
-		width: 100px;
-		color: white;
-		background-color: lightblue;
-		border: none;
-	}
-
-	#box{
-
-		background-color: grey;
-		margin: auto;
-		width: 300px;
-		padding: 20px;
-	}
-
-	</style>
-
-	<div id="box">
-		
-		<form method="post">
-			<div style="font-size: 30px;margin: 10px;color: blue;">Login to Simpleticket!</div>
-			<p> Event Name </p>
-			<input id="text" type="text" name="name"><br><br>
-			<p> Event Location </p>
-			<input id="text" type="text" name="location"><br><br>
-			<p> # of Attendees? </p>
-			<input id="number" type="number" name="tickets_available"><br><br>
-			<input id="button" type="submit" value="Login"><br><br>
-			<a href="index.php">Click to return to main page!</a><br><br>
-
+    <div id="box">
+        <h2>Create an Event!</h2>
+        <form method="post">
+            <div class="form-group">
+                <label for="name">Event Name</label>
+                <input type="text" id="name" name="name">
+            </div>
+            <div class="form-group">
+                <label for="location">Event Location</label>
+                <input type="text" id="location" name="location">
+            </div>
+            <div class="form-group">
+                <label for="tickets_available"># of Attendees</label>
+                <input type="number" id="tickets_available" name="tickets_available">
+            </div>
+            <button id="button" type="submit">Create Event</button>
+            <a href="index.php">Click to return to the main page</a>
+        </form>
+    </div>
 </body>
 </html>
