c25d2ee71bd88410e45b65eb9b388150c57329a9
sallyretro <61210853+sallyretro@users.noreply.github.com>
2023-12-06 23:18:08 -0600

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,73 +1,89 @@
 <?php
-	session_start();
-	include("serverconnection.php");
-	include("functions.php");
-	
-	if ($_SERVER['REQUEST_METHOD'] == "POST"){
+session_start();
+include("serverconnection.php");
+include("functions.php");
 
-		$username =$_POST['username'];
-		$password =$_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-		if(!empty($username) && !empty($password)){
-			$user_id = random_int(1, 9999);
-			$query = "INSERT INTO users (user_id, username, password) values('$user_id', '$username', '$password')";
-			mysqli_query($con, $query);
-			header("Location: login.php");
-			die;
-		}
-		else{
-			echo "Not an accepted combination";
-		}
-	}
+    if (!empty($username) && !empty($password)) {
+        // Hash the password before storing it in the database
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+        //prepared statements to prevent SQL injection
+        $stmt = $con->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+        $stmt->bind_param("ss", $username, $hashed_password);
+
+        if ($stmt->execute()) {
+            $stmt->close();
+            header("Location: login.php"); // Redirect to login page after successful registration
+            die();
+        } else {
+            echo "Error: " . $stmt->error;
+        }
+    } else {
+        echo "Please fill in both username and password.";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Signup to SimpleTicket!</title>
+    <title>Signup for SimpleTicket</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        #box {
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            padding: 20px;
+            text-align: center;
+            max-width: 300px;
+        }
+
+        #text, #button {
+            width: 100%;
+            padding: 10px;
+            margin: 5px 0;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            font-size: 16px;
+        }
+
+        #button {
+            background-color: #3498db;
+            color: #fff;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
+
+        #button:hover {
+            background-color: #258cd1;
+        }
+    </style>
 </head>
 <body>
-
-<style type="text/css">
-	#text{
-
-		height: 25px;
-		border-radius: 5px;
-		padding: 4px;
-		border: solid thin #aaa;
-		width: 100%;
-	}
-
-	#button{
-
-		padding: 10px;
-		width: 100px;
-		color: white;
-		background-color: lightblue;
-		border: none;
-	}
-
-	#box{
-
-		background-color: grey;
-		margin: auto;
-		width: 300px;
-		padding: 20px;
-	}
-
-	</style>
-
-	<div id="box">
-		
-		<form method="post">
-			<div style="font-size: 30px;margin: 10px;color: blue;">Signup for SimpleTicket!</div>
-			<p> Username </p>
-			<input id="text" type="text" name="username"><br><br>
-			<p> Password </p>
-			<input id="text" type="password" name="password"><br><br>
-
-			<input id="button" type="submit" value="Signup"><br><br>
-
-			<a href="login.php">Click to Login!</a><br><br>
-
+    <div id="box">
+        <form method="post">
+            <h1>Signup for SimpleTicket</h1>
+            <label for="username">Username</label>
+            <input id="text" type="text" name="username" required>
+            <label for="password">Password</label>
+            <input id="text" type="password" name="password" required>
+            <input id="button" type="submit" value="Signup">
+            <p><a href="login.php">Click to Login</a></p>
+        </form>
+    </div>
 </body>
 </html>
